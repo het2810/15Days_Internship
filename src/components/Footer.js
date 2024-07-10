4d79d257a17ccd56a80cbbf7aa2b4a8f752531ba
het2810 <93484457+het2810@users.noreply.github.com>
2024-07-10 12:45:46 +0530

    Krisha's Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,12 +15,18 @@ export const Footer = () => {
             {/* <img src={logo} alt="Logo" /> */}
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
+            
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <h4>Authors</h4>
+              Het Patel <a href="https://github.com/het2810"><img src={navIcon2} alt="Icon" /></a><br></br>
+              Harsh Lohana <a href="https://github.com/HarshilLohana"><img src={navIcon2} alt="Icon" /></a><br></br>
+              Krisha Patel <a href="https://github.com/krishapatel14"><img src={navIcon2} alt="Icon" /></a><br></br>
+              Parshwa <a href="https://github.com/parshwa9324"><img src={navIcon2} alt="Icon" /></a><br></br>
+              Pratik Bajaj <a href="https://github.com/Pratikbajaj"><img src={navIcon2} alt="Icon" /></a><br></br>
+              Viral Modi <a href="https://github.com/ViruModi228"><img src={navIcon2} alt="Icon" /></a>
+              {/* <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            
           </Col>
         </Row>
       </Container>
